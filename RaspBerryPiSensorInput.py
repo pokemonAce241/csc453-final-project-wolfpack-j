c61e4d695c3b1a5ea5217da4fee418892a953548
jdbencke <jdbencke@ncsu.edu>
2019-04-22 23:38:11 -0400

    Add files via upload
    updated raspberry pi sensor code

--- RaspBerryPiSensorInput.py
+++ RaspBerryPiSensorInput.py
@@ -18,60 +18,59 @@ import signal
 import sys, getopt
 
 
-ldr_max = 40128
 ldr_min = 2400
+ldr_max = 40128
 
-accel_max = 11
-accel_min = -11
+accel_min = -20
+accel_max = 20
+
+gyro_min = -251
+gyro_max = 251
+
+humidity_min = 0
+humidity_max = 100
+
+temp_min = 0
+temp_max = 48
+
+
 
 
 client = None
-sensor = None
-sensor2 = None
+mpu = None
+dht11 = None
 
 def main():
-    global client, sensor, sensor2
+    global client, mpu, dht11
     # register the signal handler
     signal.signal(signal.SIGINT, signal_handler)
-    status = "default"
-    # parse the command line arguments
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "i:")
-    except getopt.GetoptError:
-        print('GET OPT ERROR')
-    for opt, arg in opts:
-        if opt == '-i':
-            status = arg
 
-    sensor = mpu6050(0x68)
-    sensor2 = Adafruit_DHT.DHT11
-    
-    last_ldr_val = 0.0
-    last_pot_val = 0.0
-    
+    mpu = mpu6050(0x68)
+    dht11 = Adafruit_DHT.DHT11
+     
+      
     try:
         options = {
-            "org": "dk8so0",
-            "id": "the-raspberry-pi",
+            "org": "ml5pjc",
+            "id": "rpi",
             "auth-method": "apikey",
-            "auth-key": "a-dk8so0-ezy9ezvriv",
-            "auth-token": "7rb_hNq_l58rdDa5ZV"
+            "auth-key": "a-ml5pjc-tfl4ng9v5z",
+            "auth-token": "X0-327z9TUSLql6?dC"
         }
-        
         client = ibmiotf.application.Client(options)
-        print('s')
         client.connect()
         print("Connected to IBM Cloud!!")
 
     except ibmiotf.ConnectionException  as e:
         print(e)
+    
     print("starting the loop")
     loop()
     
 
 def loop():
-    global sensor
-    global sensor2
+    global mpu
+    global dht11
     #global ldr
     
     # create the spi bus
@@ -86,25 +85,41 @@ def loop():
     # create analog channels for the LDR and potentiometer
     ldr = AnalogIn(mcp, MCP.P0)
     #pot = AnalogIn(mcp, MCP.P1)
+    a_x = a_y = a_z = g_x = g_y = g_z = h = t = 0
+    
     while True:
         ldr_val = (ldr.value - ldr_min)/(ldr_max - ldr_min)
-        a = sensor.get_accel_data() # get the acceleration data
+        a = mpu.get_accel_data() # get the acceleration data
         a_x = (a['x'] - accel_min)/(accel_max - accel_min)
         a_y = (a['y'] - accel_min)/(accel_max - accel_min)
         a_z = (a['z'] - accel_min)/(accel_max - accel_min)
-        g = sensor.get_gyro_data() # get the gyro data
-        h, t = Adafruit_DHT.read_retry(sensor2,21)
-        myData = {'Gyro' : [g['x'], g['y'], g['z']],
-                  'Motion' : [a_x, a_y, a_z],
-                  'Light' :ldr_val,
-                  'Temp' : t,
-                  'Humidity' : h}
-        print(myData)
-        client.publishEvent("RaspberryPi", "the-raspberry-pi", "musicSensorData", "json", myData, 2)
-#        print(ldr_val)
-#        print(a)
-#        print(g)
-        time.sleep(0.1)
+        
+        g = mpu.get_gyro_data() # get the gyro data
+        g_x = (g['x'] - gyro_min)/(gyro_max - gyro_min)
+        g_y = (g['y'] - gyro_min)/(gyro_max - gyro_min)
+        g_z = (g['z'] - gyro_min)/(gyro_max - gyro_min)
+        
+       
+        
+
+        h_now, t_now = Adafruit_DHT.read(Adafruit_DHT.DHT11, 21)
+        if h_now != None and t_now != None:
+            h = (h_now - humidity_min)/(humidity_max - humidity_min)
+            t = (t_now - temp_min)/(temp_max - temp_min)
+            if h > 1:
+                h = 1
+            if t > 1:
+                t = 1
+        
+        myData = {'Gyro' : [round(g_x,2), round(g_y,2), round(g_z,2)],
+                  'Motion' : [round(a_x,2), round(a_y,2), round(a_z,2)],
+                  'Light' : round(ldr_val,2),
+                  'Temp' : round(t,2),
+                  'Humidity' : round(h,2)}
+        print(t)
+#        print(myData)
+        client.publishEvent("RaspberryPi", "rpi", "musicSensorData", "json", myData, 2)
+        time.sleep(0.01)
 
 def clean_up():
     global client
